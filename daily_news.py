c3968bb7bc741d78a3806da2a867122c83b3247a
jooossshhhh <107163609+jooossshhhh@users.noreply.github.com>
2022-08-15 09:30:49 -0500

    Added in quote of the day and weather
    Weather was already in the code, but now it is email ready. 
    QOTD has been added to email, as well.
    Comments are now more prevalent.
    Took out monkey pox cases for the time being because the websites were changing.
    Commented out final block of code because this code is now being run on a server and input is no longer needed.

--- daily_news.py
+++ daily_news.py
@@ -10,7 +10,9 @@ import requests
 import webbrowser
 from automate_email import email_alert
 
+#function to scrape information from the web using only a url and user agent
 def scrapin(url,user):
+    #get information from url
     res = requests.get(str(url),headers = user)
     res.status_code == requests.codes.ok
     #USED TO HALT A BAD DOWNLOAD
@@ -18,21 +20,26 @@ def scrapin(url,user):
         res.raise_for_status()
     except Exception as exc:
         print('There was a problem: %s' % (exc))
+    #parse the result
     res_web = BeautifulSoup(res.text,'html.parser')
     return res_web
 def webscrape(city):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-
+    #takes input and makes it usable for the google link
     city = city +' weather'
     city=city.replace(' ','+')
+    #scrape two websites
     read = scrapin(f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8',headers)
     read_news = scrapin('https://www.allsides.com/',headers)
     try:
+        #get information about the weather then pice it together for return
         location = read.select('#wob_loc')[0].getText().strip()  
         info = read.select('#wob_dc')[0].getText().strip() 
         weather = read.select('#wob_tm')[0].getText().strip()
         print(location+': ' + info + ' - ' + weather + ' degrees\n')
-
+        weather = location.replace(' ',', ')+': ' + info + ' - ' + weather + ' degrees\n'
+        
+        #make list of the hyperlinks and titles for the articles we are finding
         hyper_set=[]
         title_set = []
         #compile list of titles from the entire website
@@ -45,46 +52,45 @@ def webscrape(city):
                     hyper_set.append(url+article['href'])
     except: 
         print('invalid search')
-    return(hyper_set,title_set)
+    return(hyper_set,title_set,weather)
 
-links,titles = webscrape(city)
+links,titles,weather_info = webscrape(city)
 
 user_agent={'User-agent': 'Mozilla/5.0'}
-
-us_cases_web = scrapin('https://www.cdc.gov/poxvirus/monkeypox/response/2022/us-map.html',user_agent)
-cases_web = scrapin('https://www.monkeypoxmeter.com/',user_agent)
-
-
-center=cases_web.find_all('center')
-us_cases = us_cases_web.find('span',class_='fs12 text-primary').getText().lstrip()
-
+#find quote of the day
+quotes_web = scrapin('https://www.brainyquote.com/quote_of_the_day',user_agent)
+qotd= quotes_web.find('div',class_='grid-item qb clearfix bqQt').getText()
 
 counter = 1
 text_message = []
 email_stuff = []
 for title in zip(titles,links):
     news_print = '('+str(counter)+') '+title[0]+'\n' 
+    #make list of strings ready to be displayed in an email
     email = '<pre>\n(<a href='+str(title[1])+'>_'+str(counter)+'_</a>) ' + title[0] +'</pre>'
-    print(news_print)
+    #print(news_print)
     text_message.append(news_print)
     email_stuff.append(email)
     counter +=1
-    
 
+#make weather and quote of the day ready for email
+email_stuff.insert(0,'<pre>'+weather_info+'\n</pre>')
+email_stuff.append('<pre>'+qotd+'</pre>')
+
+#send out the email
+list_of_emails = ['blah','blahblah','blah']
 email_alert('Daily News', 'placeholder',','.join(email_stuff).replace(',',''), list_of_emails)
 
 
-print('\nU.S. Cases\n------\n' + us_cases)
-print('\n\nWorldwide\n------')
-print(center[3].getText().replace('cases','cases\n').replace(')vs',') vs')+'\n')
 
-print('Enter article number to open: \n')
-x = input()
+#only if you are running this on local computer and not a server
+#print('Enter article number to open: \n')
+#x = input()
 
-while x:
-    open_article = links[int(x)-1]
-    webbrowser.open(open_article)
-    print('Now opening... '+open_article)
-    print("\nInput another number or press enter to leave.\n")
-    x=input()
-print('Exiting...')
+#while x:
+#    open_article = links[int(x)-1]
+#    webbrowser.open(open_article)
+#    print('Now opening... '+open_article)
+#    print("\nInput another number or press enter to leave.\n")
+#    x=input()
+#print('Exiting...')
